c41f03996600d329208e4312185252bde55123a0
jarogers095 <jarogers095@gmail.com>
2020-04-28 03:51:01 -0700

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,57 @@
 class UsersController < ApplicationController
 
     get "/login" do
-        erb :"/users/login"
+        if logged_in?
+            redirect "/tweets"
+        else
+            erb :"/users/login"
+        end
     end
 
     post "/login" do
-        redirect "/tweets"
+        if params[:username] != "" && params[:password] != ""
+            @user = User.find_by(username: params[:username])
+            if @user && @user.authenticate(params[:password])
+                session[:user_id] = @user.id
+                redirect "/tweets"
+            else 
+                redirect "/login"
+            end
+        else 
+            redirect "/login"
+        end
     end
 
     get "/signup" do
-        erb :"/users/signup"
+        if logged_in?
+            redirect "/tweets"
+        else
+            erb :"/users/signup"
+        end
     end
 
     post "/signup" do
-
-        redirect "/login"
+        if params[:username] != "" && params[:email] != "" && params[:password] != ""
+            @user = User.create(params)
+            session[:user_id] = @user.id
+            redirect "/tweets"
+        else
+            redirect "/signup"
+        end
     end
 
     get "/logout" do
+        if logged_in?
+            session.clear
+            redirect "/login"
+        else
+            redirect "/"
+        end
+    end
 
-        redirect "/login"
+    get "/users/:slug" do
+        @user = User.find_by_slug(params[:slug])
+        
+        erb :"/users/show"
     end
 end
