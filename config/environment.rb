ca0d44f8645db4799c8242d49b716d90f34c20e2
Rick Belson <belsonrj@aol.com>
2020-04-30 03:23:29 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -2,6 +2,9 @@ ENV['SINATRA_ENV'] ||= "development"
 
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
+#configure :development do
+#  set :database, 'sqlite3:db/migrate.db'
+#end
 
 ActiveRecord::Base.establish_connection(ENV['SINATRA_ENV'].to_sym)
 
---
+++ db/migrate/20200429220749_change_password_column.rb
@@ -0,0 +1,5 @@
+class ChangePasswordColumn < ActiveRecord::Migration[4.2]
+  def change
+    rename_column :users, :password, :password_digest
+  end
+end
