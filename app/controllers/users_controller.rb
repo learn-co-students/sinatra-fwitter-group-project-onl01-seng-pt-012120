366b0929c0a1c9d14853c4922cecb2ca92134965
N-Santiago <norbertosantiago82@gmail.com>
2020-05-03 10:06:39 -0800

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,46 @@
 class UsersController < ApplicationController
 
-    get '/' do
-        erb :index
-      end
-
-    helpers do
-	    def logged_in?
-			!!session[:user_id]
+    get '/signup' do
+		if Helpers.is_logged_in?(session)
+		  redirect to '/tweets'
 		end
-
-		def current_user
-			User.find(session[:user_id])
+		erb :'/users/createuser'
+	  end
+	
+	  post '/signup' do
+		if !(params.has_value?(""))
+		  user = User.create(params)
+		  session[:user_id] = user.id
+		  redirect to '/tweets'
+		else
+		  redirect to '/signup'
 		end
-	end  
+	  end
+	
+	  get '/login' do
+		if Helpers.is_logged_in?(session)
+		  redirect to '/tweets'
+		end
+		erb :'/users/login'
+	  end
+	
+	  post '/login' do
+	  user = User.find_by(:username => params[:username])
+		  if user && user.authenticate(params[:password])
+		  session[:user_id] = user.id
+		  redirect to "/tweets"
+		else
+		  redirect to "/login"
+		end
+		end
+	
+	  get '/logout' do
+		if Helpers.is_logged_in?(session)
+		  session.clear
+		else
+		  redirect to '/'
+		end
+		redirect to '/login'
+	  end
 
 end
