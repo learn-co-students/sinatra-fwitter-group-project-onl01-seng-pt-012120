312a27448d53b9d484a631236cd2dc91dd76a7f5
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-05-11 22:02:09 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,12 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "test"
+  end
+
+  get "/" do
+   erb :layout
   end
 
 end
