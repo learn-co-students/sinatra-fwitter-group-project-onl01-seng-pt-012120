ecc227426a5f7b2eef79d51fd30f268ef83f40b4
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-05-12 17:49:05 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,19 @@ class ApplicationController < Sinatra::Base
   end
 
   get "/" do
-   erb :layout
+   erb :index
+  end
+
+  helpers do
+
+    def logged_in?
+      !!current_user
+    end
+
+    def current_user
+      @current_user ||= User.find_by(session[:user_id]) if session[:user_id]
+    end
+
   end
 
 end
