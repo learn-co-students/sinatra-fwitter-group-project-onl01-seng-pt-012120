d3f6ac93357bb8283541055ab12aba49d211f8ce
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-05-12 18:09:28 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,39 +1,44 @@
 class TweetsController < ApplicationController
-
   get '/tweets' do
     if logged_in?
       @tweets = Tweet.all
-      erb :"tweets/index"
+      erb :'tweets/tweets'
     else
-      redirect to "/login"
+      redirect to '/login'
     end
   end
 
   get '/tweets/new' do
     if logged_in?
-      erb :"tweets/new"
+      erb :'tweets/create_tweet'
     else
-      redirect "/login"
+      redirect to '/login'
     end
   end
 
-  post "/tweets" do
-    if params[:content] != ""
-      @tweet = Tweet.new(params)
-      @tweet.user = current_user
-      @tweet.save
-      redirect "/tweets/#{@tweet.id}"
+  post '/tweets' do
+    if logged_in?
+      if params[:content] == ""
+        redirect to "/tweets/new"
+      else
+        @tweet = current_user.tweets.build(content: params[:content])
+        if @tweet.save
+          redirect to "/tweets/#{@tweet.id}"
+        else
+          redirect to "/tweets/new"
+        end
+      end
     else
-      redirect "/tweets/new"
+      redirect to '/login'
     end
   end
 
-  get "/tweets/:id" do
+  get '/tweets/:id' do
     if logged_in?
       @tweet = Tweet.find_by_id(params[:id])
-      erb :"/tweets/show"
+      erb :'tweets/show_tweet'
     else
-      redirect "/login"
+      redirect to '/login'
     end
   end
 
@@ -41,33 +46,45 @@ class TweetsController < ApplicationController
     if logged_in?
       @tweet = Tweet.find_by_id(params[:id])
       if @tweet && @tweet.user == current_user
-        erb :"/tweets/edit"
+        erb :'tweets/edit_tweet'
+      else
+        redirect to '/tweets'
       end
     else
-      redirect "/login"
+      redirect to '/login'
     end
   end
 
- patch "/tweet/:id" do
-   @tweet = Tweet.find_by_id(params[:id])
-   if params[:content] != ""
-     @tweet.update(content: params[:content])
-     redirect "/tweets/#{@tweet.id}"
-   else
-     redirect "/tweets/#{@tweet.id}/edit"
-   end
- end
+  patch '/tweets/:id' do
+    if logged_in?
+      if params[:content] == ""
+        redirect to "/tweets/#{params[:id]}/edit"
+      else
+        @tweet = Tweet.find_by_id(params[:id])
+        if @tweet && @tweet.user == current_user
+          if @tweet.update(content: params[:content])
+            redirect to "/tweets/#{@tweet.id}"
+          else
+            redirect to "/tweets/#{@tweet.id}/edit"
+          end
+        else
+          redirect to '/tweets'
+        end
+      end
+    else
+      redirect to '/login'
+    end
+  end
 
- delete "/tweets/:id/delete" do
-   if logged_in?
-     @tweet = Tweet.find_by_id(params[:id])
-     if @tweet && @tweet.user == current_user
-       @tweet.delete
-     end
-     redirect "/tweets"
-   else
-     redirect "/login"
-   end
- end
-
+  delete '/tweets/:id/delete' do
+    if logged_in?
+      @tweet = Tweet.find_by_id(params[:id])
+      if @tweet && @tweet.user == current_user
+        @tweet.delete
+      end
+      redirect to '/tweets'
+    else
+      redirect to '/login'
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_12_015301) do
+ActiveRecord::Schema.define(version: 2020_05_11_213101) do
 
   create_table "tweets", force: :cascade do |t|
     t.string "content"
