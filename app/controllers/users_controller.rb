ce06b2c49370414df1a632170a91ae4a2388ee8f
Beth Ruff <beth.ruff2@gmail.com>
2020-05-13 19:35:18 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,55 +1,53 @@
 class UsersController < ApplicationController
-
-  get '/' do 
-   erb :homepage
-  end 
   
   get '/signup' do
-    if logged_in?
-      redirect to '/tweets'
-    erb :"/users/signup" 
+    if Helpers.is_logged_in?(session)
+      redirect '/tweets'
+    else 
+      erb :'users/signup'
     end 
   end 
   
   post '/signup' do 
     if params[:username] == "" || params[:email] == "" || params[:password] == ""
-      redirect to '/signup'
-    else
-      @user = User.create(username: params[:username], email: params[:email], password: params[:password])
-      session[:user_id] = @user.id 
-      redirect to '/tweets'
+      redirect 'users/signup'
+    end 
+  
+      user = User.create(params)
+      if user && user.authenticate(params[:password])
+      session[:user_id] = user.id 
+      redirect '/tweets'
     end 
   end 
   
   get '/login' do 
-    if logged_in?
-      redirect to '/tweets'
-    erb :'/users/login'
+    if Helpers.is_logged_in?(session)
+      redirect '/tweets'
+    else 
+      erb :'/users/login'
     end 
   end 
   
   post '/login' do 
-    @user = User.find_by(username: params[:username])
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      redirect to '/tweets'
-    else
-      redirect to '/signup'
-    end 
-  end 
-  
-  get '/users/:slug' do
-    @user = User.find_by_slug(params[:slug])
-    erb :'/users/show'
+    user = User.find_by(username: params[:username])
+   	  if user && user.authenticate(params[:password])
+	    session[:user_id] = user.id
+	    end
+    redirect '/tweets'
   end
   
   get '/logout' do 
-    if logged_in?
+    if Helpers.is_logged_in?(session)
       session.clear
-        redirect to '/login'
+        redirect 'users/login'
     else
-      redirect to '/tweets'
+      redirect '/tweets'
     end
   end 
+    
+  get '/users/:slug' do
+    @user = User.find_by_slug(params[:slug])
+    erb :'users/show'
+  end
 
 end
